eeae40f77e0cbfa8ebce6a8b5be2588aace00199
Raphael Nunes <47608429+raphaelnunes67@users.noreply.github.com>
2023-02-27 18:41:01 -0400

    Update CRC16.py

--- CRC16.py
+++ CRC16.py
@@ -1,4 +1,16 @@
-def MODBUS_CRC16(buf, length):
+def MODBUS_CRC16_v1(buf, length):
+    crc = 0xFFFF
+    for i in range(length):
+        crc ^= buf[i]
+        for bit in range(8):
+            if crc & 0x0001:
+                crc >>= 1
+                crc ^= 0xA001
+            else:
+                crc >>= 1
+    return crc
+
+def MODBUS_CRC16_v2(buf, length):
     table = [0x0000, 0xA001]
     crc = 0xFFFF
     for i in range(length):
@@ -8,11 +20,11 @@ def MODBUS_CRC16(buf, length):
             crc >>= 1
             crc ^= table[xor]
     return crc
-
+    
 
 if __name__ == '__main__':
     
     buf = bytearray([0x5a, 0x00, 0x3f, 0x01, 0x00])
-    crc = MODBUS_CRC16_v2(buf, len(buf))
+    crc = MODBUS_CRC16_v1(buf, len(buf))
 
     print('CRC: {}'.format(hex(crc)))
